3232f120cfa9b45ce0cdfd11b6cfe890b6066ca4
Agnieszka Łupińska <agnieszka.gesiewska@o2.pl>
2025-01-14 02:23:56 +0100

    Add eventClick

--- js/script.js
+++ js/script.js
@@ -6,13 +6,11 @@
   }); */
 
   const titleClickHandler = function(event){
+    event.preventDefault();
+    const clickedElement = this;
     console.log('Link was clicked!');
 
     /* remove class 'active' from all article links  */
-
-    /* add class 'active' to the clicked link */
-
-    /* remove class 'active' from all articles */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -20,11 +18,33 @@
       activeLink.classList.remove('active');
     }
 
+    /* add class 'active' to the clicked link */
+
+    clickedElement.classList.add('active');
+    console.log('clickedElement:',clickedElement);
+
+    /* remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.post.active');
+    for (let activeArticle of activeArticles) {
+      activeArticle.classList.remove('active');
+    }
+
     /* get 'href' attribute from the clicked link */
+
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log('articleSelector',articleSelector);
 
     /* find the correct article using the selector (value of 'href' attribute) */
 
+    const targetArticle = document.querySelector(articleSelector);
+    console.log('targetArticle',targetArticle);
+
     /* add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
+
+
   }
 
   const links = document.querySelectorAll('.titles a');
